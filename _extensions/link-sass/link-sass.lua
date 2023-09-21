eefc5904f69197b8f0ff207c7d14c189c01f47b5
andrewpbray <andrewbray@berkeley.edu>
2023-09-21 16:43:02 -0700

    adjust comments

--- _extensions/link-sass/link-sass.lua
+++ _extensions/link-sass/link-sass.lua
@@ -15,6 +15,7 @@ local function string_to_table(str)
   return(lines)
 end
 
+-- recursive function to resolve variables that reference other variables
 local function resolve_ref(v, tab)
   local v_out = nil
   local var_ref = string.match(v, '%$[%w%-_]+')
@@ -35,6 +36,7 @@ local function resolve_ref(v, tab)
   return(v_out)
 end
 
+
 --================--
 -- Core Functions --
 --================--
@@ -50,7 +52,7 @@ function fetch_sass(meta)
   
   sass_vars = {}
   
-  -- Create table of sass variables as they exist in the theme file
+  -- create table of sass variables as they exist in the theme file
   for _,line_text in ipairs(scss_tab) do
     if string.match(line_text, "scss:rules") ~= nil then
       is_sass_sec = false
@@ -74,7 +76,7 @@ function fetch_sass(meta)
     sass_vars[k] = string.gsub(no_default, "%s*!important", "")
   end
   
-  -- Resolve all relative references (i.e. no $theme-blue: $blue)
+  -- resolve all relative references (i.e. no $theme-blue: $blue)
   for k,v in pairs(sass_vars) do
     if string.sub(v, 1, 1) == "$" then
       sass_tab[k] = resolve_ref(v, sass_vars)
@@ -86,16 +88,21 @@ function fetch_sass(meta)
   return meta
 end
 
-
 function replace_with_sass(block_or_inline)
+  
+  -- scan through doc for attributes that look like sass variables
   for _,el in ipairs(block_or_inline) do
     if el.attributes ~= nil then
+      
       for k,v in pairs(el.attributes) do
         sass_var = string.match(v, "(%$[%w_%-]+)")
+        
+        -- if attribute is sassy, swap with the value of the var from the scss file
         if sass_var ~= nil then
           el.attr.attributes[k] = string.gsub(v, "(%$[%w_%-]+)", sass_tab[sass_var])
         end
       end
+      
     end
   end
   
